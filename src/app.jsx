3081d410ffd76d004175cf1455536c9884883962
Naataanii Tsosie <naataaniitsosie@gmail.com>
2026-02-12 21:54:39 -0700

    basic routing with react

--- src/app.jsx
+++ src/app.jsx
@@ -1,25 +1,44 @@
 import React from 'react';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { Login } from './login/login';
+import { Punch } from './punch/punch';
+import { History } from './history/history';
+import { Admin } from './admin/admin';
+import { Help } from './help/help';
+
 export default function App() {
-  return <div className="body">
-    <header class="flex flex-row justify-between items-center">
-        <div class="flex-0"><a href="/"><img src="logo.png" width="150" alt="Time Ninja Logo" /></a></div>
-        <nav class="flex flex-1 flex-row gap-4">
-            <div class="flex-1 text-center"><a href="/">Home</a></div>
-            <div class="flex-1 text-center"><a href="punch.html">Punch</a></div>
-            <div class="flex-1 text-center"><a href="history.html">History</a></div>
-            <div class="flex-1 text-center"><a href="admin.html">Admin</a></div>
-            <div class="flex-1 text-center"><a href="help.html">Help</a></div>
-        </nav>
-    </header>
-    <main>App components go here</main>
-    <footer class="m-5 mt-20 text-center">
-        <div><a class="underline" href="https://github.com/naataaniitsosie/startup">GitHub</a></div>
-        <div>Naataanii Tsosie - CS 260</div>
-        <div>Time Ninja - <span>Copyright 2026</span> </div>
-        <div id="footer-chuck-norris">Chuck Norris Joke provided by <a class="underline" href="https://api.chucknorris.io/">Chuck Norris API</a>: Some really funny joke will go here...</div>
-    </footer>
-  </div>;
+  return (
+    <BrowserRouter>
+        <div className="body">
+            <header className="flex flex-row justify-between items-center">
+                <div className="flex-0"><a href="/"><img src="logo.png" width="150" alt="Time Ninja Logo" /></a></div>
+                <nav className="flex flex-1 flex-row gap-4">
+                    <div className="flex-1 text-center"><NavLink className="nav-link" to="/">Login</NavLink></div>
+                    <div className="flex-1 text-center"><NavLink className="nav-link" to="/punch">Punch</NavLink></div>
+                    <div className="flex-1 text-center"><NavLink className="nav-link" to="/history">History</NavLink></div>
+                    <div className="flex-1 text-center"><NavLink className="nav-link" to="/admin">Admin</NavLink></div>
+                    <div className="flex-1 text-center"><NavLink className="nav-link" to="/help">Help</NavLink></div>
+                </nav>
+            </header>
+            <main>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/punch" element={<Punch />} />
+                <Route path="/history" element={<History />} />
+                <Route path="/admin" element={<Admin />} />
+                <Route path="/help" element={<Help />} />
+            </Routes>
+            </main>
+            <footer className="m-5 mt-20 text-center">
+                <div><a className="underline" href="https://github.com/naataaniitsosie/startup">GitHub</a></div>
+                <div>Naataanii Tsosie - CS 260</div>
+                <div>Time Ninja - <span>Copyright 2026</span> </div>
+                <div id="footer-chuck-norris">Chuck Norris Joke provided by <a className="underline" href="https://api.chucknorris.io/">Chuck Norris API</a>: Some really funny joke will go here...</div>
+            </footer>
+        </div>
+    </BrowserRouter>
+  )
 }
