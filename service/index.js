f263f3f4c3f733a60167e4c66ba6b393037dc64d
Naataanii Tsosie <naataaniitsosie@gmail.com>
2026-03-12 16:54:47 -0600

    add nodemon for fast local development

--- service/index.js
+++ service/index.js
@@ -65,6 +65,9 @@ apiRouter.delete('/auth/logout', async (req, res) => {
 
 // Middleware to verify that the user is authorized to call an endpoint
 const verifyAuth = async (req, res, next) => {
+  if (process.env.DISABLE_AUTH === 'true') {
+    return next();
+  }
   const user = await findUser('token', req.cookies[authCookieName]);
   req.user = user
   if (user) {
