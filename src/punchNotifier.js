e1aff478a9269c4ae8d4c9f72ca1c7ad5c9cc852
Naataanii Tsosie <naataaniitsosie@gmail.com>
2026-04-06 19:24:34 -0600

    add punch notifier

---
+++ src/punchNotifier.js
@@ -0,0 +1,62 @@
+const PunchEvent = {
+  System: 'system',
+  In: 'in',
+  Out: 'out',
+};
+
+class EventMessage {
+  constructor(from, type, value) {
+    this.from = from;
+    this.type = type;
+    this.value = value;
+  }
+}
+
+class PunchEventNotifier {
+  events = [];
+  handlers = [];
+
+  constructor() {
+    let port = window.location.port;
+    const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+    this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
+    this.socket.onopen = (event) => {
+      this.receiveEvent(new EventMessage('TimeNinja', PunchEvent.System, { msg: 'connected' }));
+    };
+    this.socket.onclose = (event) => {
+      this.receiveEvent(new EventMessage('TimeNinja', PunchEvent.System, { msg: 'disconnected' }));
+    };
+    this.socket.onmessage = async (msg) => {
+      try {
+        const event = JSON.parse(await msg.data.text());
+        this.receiveEvent(event);
+      } catch {}
+    };
+  }
+
+  broadcastEvent(from, type, value) {
+    const event = new EventMessage(from, type, value);
+    this.socket.send(JSON.stringify(event));
+  }
+
+  addHandler(handler) {
+    this.handlers.push(handler);
+  }
+
+  removeHandler(handler) {
+    this.handlers = this.handlers.filter((h) => h !== handler);
+  }
+
+  receiveEvent(event) {
+    this.events.push(event);
+
+    this.events.forEach((e) => {
+      this.handlers.forEach((handler) => {
+        handler(e);
+      });
+    });
+  }
+}
+
+const PunchEventNotifier = new PunchEventNotifier();
+export { PunchEvent, PunchEventNotifier };
