ac1aec863fcd5a185a81008dc8acb9d888b925c5
Naataanii Tsosie <naataaniitsosie@gmail.com>
2026-04-06 19:19:10 -0600

    add backend ws

--- service/index.js
+++ service/index.js
@@ -5,6 +5,7 @@ const uuid = require('uuid');
 const app = express();
 const DB = require('./database.js');
 
+const { peerProxy } = require('./peerProxy.js');
 
 const authCookieName = 'token';
 
@@ -204,6 +205,8 @@ function setAuthCookie(res, authToken) {
   });
 }
 
-app.listen(port, () => {
+const httpService =app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+peerProxy(httpService);
